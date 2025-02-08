7b7b6260a8069e543475e7875fc13cffe4052873
henrique.roos <henrique.roos@egadnet.com.br>
2025-02-08 11:30:00 -0300

    first commit

---
+++ src/main/java/com/investment_aggregator/insvestment_aggregator/InsvestmentAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.investment_aggregator.insvestment_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsvestmentAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsvestmentAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investment_aggregator/insvestment_aggregator/InsvestmentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment_aggregator.insvestment_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsvestmentAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
